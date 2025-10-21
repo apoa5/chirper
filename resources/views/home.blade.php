bd68e32bac9978cacec0ac1e2139b4f529f1ecb8
Akwasi <122885868+apoa5@users.noreply.github.com>
2025-10-21 10:09:16 +0000

    Created chirps migration

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,9 +1,10 @@
+
 <x-layout>
     <x-slot:title>
         Welcome
     </x-slot:title>
     <div class="max-w-2xl mx-auto">
-        @foreach ($chirps as $chirp )
+        @foreach ($chirps as $chirp)
             <div class="card bg-base-100 shadow mt-8">
                 <div class="card-body">
                     <div>
